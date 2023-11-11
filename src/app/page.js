09288c2fcbabd711f3756d74b091612368eef6c7
Daniel de la Jara <ddelajara@gmail.com>
2023-11-11 10:02:22 -0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -6,8 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
+          Daniel Rodrigo De la Jara Jimenez&nbsp;
         </p>
         <div>
           <a
@@ -15,70 +14,81 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{' '}
             <Image
-              src="/vercel.svg"
+              src="/image/mern.png"
               alt="Vercel Logo"
               className={styles.vercelLogo}
-              width={100}
-              height={24}
+              width={170}
+              height={70}
               priority
             />
           </a>
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.authorContainer}>
+        <div className={`${styles.centerx} ${styles.circleContainer}`}>
+          <Image
+            className={styles.logo}
+            src="/image/autor.jpg"
+            alt="Next.js Logo"
+            width={240}
+            height={220}
+            priority
+          />
+        </div>
+        <div className={styles.authorText}>
+          <h1>Profesional TI @ Full Stack</h1>
+          <h3>
+          Consultoria Servicios TI / Infraestructura / Desarrollo Web FullStack / Database
+          </h3>
+          <br></br>
+          <p>
+          Ejecutivo gerencial del área TI, cuento con amplia experiencia en administración de infraestructura, soporte y desarrollo de aplicaciones.
+          </p>
+        </div>
       </div>
 
       <div className={styles.grid}>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          className={`${styles.card} ${styles.cardBG1}`}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Docs <span>-&gt;</span>
+            Perfil
           </h2>
           <p>Find in-depth information about Next.js features and API.</p>
         </a>
 
         <a
           href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+           className={`${styles.card} ${styles.cardBG2}`}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Learn <span>-&gt;</span>
+            Competencias
           </h2>
           <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
         </a>
 
         <a
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          className={`${styles.card} ${styles.cardBG3}`}
           target="_blank"
           rel="noopener noreferrer"
         >
           <h2>
-            Templates <span>-&gt;</span>
+            Proyectos
           </h2>
           <p>Explore starter templates for Next.js.</p>
         </a>
 
         <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
+          className={`${styles.card} ${styles.cardBG4}`}
           target="_blank"
           rel="noopener noreferrer"
         >
