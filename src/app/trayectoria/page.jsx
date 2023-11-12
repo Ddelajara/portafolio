c11b62cf439c6cb09435213698d137d85dd292e2
Daniel de la Jara <ddelajara@gmail.com>
2023-11-12 17:56:43 -0300

    Actualización Page Trayectoria y estilos

--- src/app/trayectoria/page.jsx
+++ src/app/trayectoria/page.jsx
@@ -114,60 +114,35 @@ const Trayectoria = () => {
             <p> <strong>2012 - 2017</strong></p>
             <p>Sub Gerente de Adm y Soporte</p>
           </a>
-        </div>
 
-  
-        <div className={styles.grid}>
-          <a
-            href="https://www.linkedin.com/in/daniel-de-la-jara-5b66a5155/"
-            className={`${styles.card} ${styles.cardBG1}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Linked In
-            </h2>
-            <p>habilidades, experiencia y competencias laborales</p>
-          </a>
-  
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-             className={`${styles.card} ${styles.cardBG2}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Trayectoria
-            </h2>
-            <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-          </a>
-  
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={`${styles.card} ${styles.cardBG3}`}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Proyectos <span>-&gt;</span>
-            </h2>
-            <p>Explore starter templates for Next.js.</p>
-          </a>
-  
+
+
           <a
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            className={`${styles.card} ${styles.cardBG4}`}
+            className={styles.card_Center}
             target="_blank"
             rel="noopener noreferrer"
           >
+            <div>
+            <Image
+                src="/image/numero-5.png"
+                alt="Number one"
+                width={60}
+                height={60}
+                priority
+                />
+            </div>
             <h2>
-              Deploy <span>-&gt;</span>
+              Proredes <span>-&gt;</span>
             </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL with Vercel.
-            </p>
+            <p> <strong>2018 - 2023</strong></p>
+            <p>Gerente de Operaciones y Servicios</p>
           </a>
+
+
+
         </div>
+
       </main>
   
     )
