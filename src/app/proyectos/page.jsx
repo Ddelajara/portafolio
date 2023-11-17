0db7abcc4c1c2274e27ea08e5fb9e8ddddd1b964
Daniel de la Jara <ddelajara@gmail.com>
2023-11-16 22:08:10 -0300

    color

--- src/app/proyectos/page.jsx
+++ src/app/proyectos/page.jsx
@@ -61,7 +61,7 @@ const Proyectos = () =>{
           </div>
 
           <div className="relative pl-16">
-            <dt className="text-base font-semibold leading-7 text-gray-900 vercelLogo">
+            <dt className="text-base font-semibold leading-7 text-gray-900">
               <div className="absolute left-0 top-0 flex h-10 w-10 items-center justify-center rounded-lg bg-green-300">
                 <Image 
                   src="image/database-line-icon.svg" // Ruta relativa al archivo en la carpeta public
@@ -71,7 +71,7 @@ const Proyectos = () =>{
                   className="h-6 w-6 text-white" // Clases para estilizar
                 />
               </div>
-              Base de Datos
+              <div className="vercelLogo"> Base de Datos</div>
             </dt>
             <dd className="mt-2 text-base leading-7 text-gray-600">
               SQL Server, Oracle, Firebase, MongoDB. Querys DML, Procedimientos Almancenados, modelamiento.
